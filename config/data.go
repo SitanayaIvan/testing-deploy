f771ab7760b4e6d026722ad4d5f690c820108dc9
SitanayaIvan <109928015+SitanayaIvan@users.noreply.github.com>
2023-01-16 21:58:06 +0700

    deploy

--- config/data.go
+++ config/data.go
@@ -6,11 +6,7 @@ type ConfigBody struct {
 }
 
 type DBAccount struct {
-	Host     string
-	Name     string
-	User     string
-	Password string
-	Port     string
+	DBUrl string
 }
 
 type AppAccount struct {
--- database/connect.go
+++ database/connect.go
@@ -9,8 +9,7 @@ import (
 func ConnectDB() (*gorm.DB, error) {
 	conf, _ := config.LoadVariable()
 
-	dsn := "host=" + conf.Db.Host + " user=" + conf.Db.User + " password=" + conf.Db.Password + " dbname=" + conf.Db.Name + " port=" + conf.Db.Port + " sslmode=disable TimeZone=Asia/Shanghai"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{SkipDefaultTransaction: true})
+	db, err := gorm.Open(postgres.Open(conf.Db.DBUrl), &gorm.Config{SkipDefaultTransaction: true})
 
 	return db, err
 }
