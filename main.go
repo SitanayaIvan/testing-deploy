64e7c003b650beb72faff04c54d73c47f46338f8
SitanayaIvan <109928015+SitanayaIvan@users.noreply.github.com>
2023-01-16 09:52:04 +0700

    deploy

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	r := gin.Default()
 	r.GET("api/test", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello World!",
+			"message": "Hello World!!!",
 		})
 	})
 	r.Run(":" + conf.Port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
